3ddc210274a61d7cbfdc2f0ae467180673e8b71a
Norma Rosita Damayanthi <114063067+normarosita16@users.noreply.github.com>
2022-10-07 03:26:37 +0700

    Update todoitem.js

--- app/models/todoitem.js
+++ app/models/todoitem.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "TodoItem",
-      tableName: "todoitems",
+      tableName: "todos",
     }
   );
   return TodoItem;
