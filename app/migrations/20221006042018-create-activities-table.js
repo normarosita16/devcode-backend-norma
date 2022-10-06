549a4cc6f73615736343b3b4d56745395ee56f69
Norma Rosita Damayanthi <114063067+normarosita16@users.noreply.github.com>
2022-10-07 03:44:02 +0700

    Update 20221006042018-create-activities-table.js

--- app/migrations/20221006042018-create-activities-table.js
+++ app/migrations/20221006042018-create-activities-table.js
@@ -1,1 +1,47 @@
-lala
+"use strict";
+
+module.exports = {
+
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable("activities", {
+
+      id: {
+
+        type: Sequelize.INTEGER,
+
+        primaryKey: true,
+
+        autoIncrement: true,
+
+        allowNull: false,
+
+      },
+
+      
+
+      title: { type: Sequelize.STRING },
+
+      
+
+      
+
+      createdAt: { type: Sequelize.DATE, allowNull: false },
+
+      updatedAt: { type: Sequelize.DATE, allowNull: false },
+
+      deletedAt: { type: Sequelize.DATE },
+
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable("activities");
+
+  },
+
+};
+
+
