713b80bc81296b6045a083a6a30e5fcf549de136
Norma Rosita Damayanthi <114063067+normarosita16@users.noreply.github.com>
2022-10-07 04:10:59 +0700

    Update activity-group-route.js

--- app/routes/activity-group-route.js
+++ app/routes/activity-group-route.js
@@ -1,1 +1,15 @@
-lala
+const dotEnv = require("dotenv");
+const { Router } = require("express");
+
+const activityGroupController = require("../controllers/activity-group-controller");
+
+const router = Router();
+dotEnv.config();
+
+router.post("", activityGroupController.create);
+router.get("", activityGroupController.list);
+router.get("/:id", activityGroupController.view);
+router.patch("/:id", activityGroupController.update);
+router.delete("/:id", activityGroupController.delete);
+
+module.exports = router;
